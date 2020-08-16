a4a7fa32302a757018070fc7297cd7e9c2cea403
manami <manami.davidson@hotmail.com>
2020-08-16 11:11:51 +1000

    complete HighScores exercism

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -7,7 +7,7 @@ To get started with TDD, see the `README.md` file in your
 =end
 
 class ResistorColorDuo
-  COLORS = %w(black brown red orange yellow green blue violet grey white).freeze
+  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
   def self.value(data)
     tens, ones = data
     COLORS.index(tens) * 10 + COLORS.index(ones)
